630cd252698e6217a071771cc3795aa77c3bdf0c
Stefan Machmeier <stefan-machmeier@outlook.com>
2024-03-22 14:27:01 +0100

    Fix name in readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'heidgaf'
+copyright = '2024, Stefan Machmeier'
+author = 'Stefan Machmeier'
 
 release = '0.1'
 version = '0.1.0'
