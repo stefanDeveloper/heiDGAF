bcab8f11f8cd733a1222649fd2e833259557b2a9
Stefan Machmeier <stefan-machmeier@outlook.com>
2024-03-25 15:13:00 +0100

    Update docs for api

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,12 +12,29 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx_autodoc_typehints',
+    'sphinxcontrib.apidoc',  # automatically generate API docs, see https://github.com/rtfd/readthedocs.org/issues/1139
+    'nbsphinx',
+    'myst_parser',
+    'sphinx_design',
 ]
+
+# -- apidoc settings ---------------------------------------------------------
+apidoc_module_dir = '../../heidgaf'
+apidoc_output_dir = 'api'
+apidoc_excluded_paths = ['**/*test*']
+apidoc_module_first = True
+apidoc_separate_modules = True
+apidoc_extra_args = ['-d 6']
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
@@ -33,3 +50,5 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+language = "en"
