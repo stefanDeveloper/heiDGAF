f91d9f8c27d7133cb29b1205f4859f92798954c7
lamr02n <94921205+lamr02n@users.noreply.github.com>
2024-05-17 12:47:20 +0200

    change producer sandbox code (not usable)

--- pipeline-prototype/sandbox/producer.py
+++ pipeline-prototype/sandbox/producer.py
@@ -1,7 +1,11 @@
 from kafka import KafkaProducer
+from kafka.admin import KafkaAdminClient, NewTopic
+
+admin_client = KafkaAdminClient(bootstrap_servers="localhost:9999")
+admin_client.create_topics(new_topics=[NewTopic(name='test_topic', num_partitions=1, replication_factor=1)])
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9999'], api_version=(1, 3, 5))
 print("Producer created")
 
-future = producer.send('test_topic', b'some_message')
+producer.send('test_topic', b'some_message')
 print("Message sent")
