eaf3cadcbbc96c046c2ee19848682a5daf9d4738
lamr02n <94921205+lamr02n@users.noreply.github.com>
2024-06-30 20:38:15 +0200

    adding log messages for batch_handler.py

--- heidgaf_core/batch_handler.py
+++ heidgaf_core/batch_handler.py
@@ -4,9 +4,8 @@ import os  # needed for Terminal execution
 import sys  # needed for Terminal execution
 from threading import Timer, Lock
 
-from heidgaf_core.kafka_handler import KafkaProduceHandler
-
 sys.path.append(os.getcwd())  # needed for Terminal execution
+from heidgaf_core.kafka_handler import KafkaProduceHandler
 from heidgaf_core.log_config import setup_logging
 from heidgaf_core.config import *
 
@@ -16,6 +15,7 @@ logger = logging.getLogger(__name__)
 
 class KafkaBatchSender:
     def __init__(self, topic: str, transactional_id: str, buffer: bool = False):
+        logger.debug(f"Initializing {self.__class__.__name__} ({topic=}, {transactional_id=} and {buffer=})...")
         self.topic = topic
         self.latest_messages = []
         self.earlier_messages = []
@@ -23,40 +23,69 @@ class KafkaBatchSender:
         self.lock = Lock()
         self.timer = None
         self.kafka_produce_handler = KafkaProduceHandler(transactional_id=transactional_id)
+        logger.debug(f"Initialized {self.__class__.__name__} ({topic=}, {transactional_id=} and {buffer=}).")
 
     def add_message(self, message: str):
+        logger.debug(f"Adding message '{message}' to batch.")
         with self.lock:
             self.latest_messages.append(message)
 
             if len(self.latest_messages) >= BATCH_SIZE:
+                logger.debug("Batch is full. Calling _send_batch()...")
                 self._send_batch()
             elif not self.timer:
+                logger.debug("Timer not set yet. Calling _reset_timer()...")
                 self._reset_timer()
+        logger.debug(f"Message '{message}' successfully added to batch.")
 
-    def close(self):
+    def close(self):  # TODO: Change to __del__
+        logger.debug(f"Closing {self.__class__.__name__} ({self.topic=} and {self.buffer=})...")
         if self.timer:
+            logger.debug("Timer is active. Cancelling timer...")
             self.timer.cancel()
+            logger.debug("Timer cancelled.")
 
+        logger.debug("Calling _send_batch()...")
         self._send_batch()
+        logger.debug(f"Closed {self.__class__.__name__} ({self.topic=} and {self.buffer=}).")
 
     def _send_batch(self):
+        logger.debug("Starting to send the batch...")
         with self.lock:
             if self.earlier_messages or self.latest_messages:
+                logger.debug("Messages not empty. Trying to send batch to KafkaProduceHandler...")
                 self.kafka_produce_handler.send(
                     topic=self.topic,
                     data=json.dumps(self.earlier_messages + self.latest_messages),
                 )
 
                 if self.buffer:
+                    logger.debug("Storing earlier messages in buffer...")
                     self.earlier_messages = self.latest_messages
+                    logger.debug("Earlier messages stored in buffer.")
 
                 self.latest_messages = []
+            else:
+                logger.debug("Messages are empty. Nothing to send.")
 
+            logger.debug("Resetting timer...")
             self._reset_timer()
+        logger.debug("Batch successfully sent or nothing to send.")
 
     def _reset_timer(self):
+        logger.debug("Resetting timer...")
         if self.timer:
+            logger.debug("Cancelling active timer...")
             self.timer.cancel()
+        else:
+            logger.debug("No timer active.")
 
+        logger.debug("Starting new timer...")
         self.timer = Timer(BATCH_TIMEOUT, self._send_batch)
         self.timer.start()
+        logger.debug("Successfully started new timer.")
+
+
+if __name__ == '__main__':
+    instance = KafkaBatchSender("topic", "tr_id")
+    instance._send_batch()
--- heidgaf_core/log_config.py
+++ heidgaf_core/log_config.py
@@ -1,39 +1,11 @@
-import inspect
 import logging
 
 import colorlog
 
-DEBUG = False
-
-
-def find_class_name():
-    current_frame = inspect.currentframe()
-    try:
-        # Walk up the call stack to find the caller's frame
-        for _ in range(3):  # Skip this function, logging function, and logger method
-            if current_frame.f_back is None:
-                return None
-            current_frame = current_frame.f_back
-
-        # Check if the frame is within a class method
-        if 'self' in current_frame.f_locals:
-            cls_name = current_frame.f_locals['self'].__class__.__name__
-
-            return cls_name if cls_name != 'Logger' else None
-        return None
-    finally:
-        del current_frame
-
-
-class ClassNameLogRecord(logging.LogRecord):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.classname = find_class_name()
+DEBUG = True
 
 
 def setup_logging():
-    logging.setLogRecordFactory(ClassNameLogRecord)
-
     log_colors = {
         'DEBUG': 'cyan',
         'INFO': 'green',
@@ -42,40 +14,25 @@ def setup_logging():
         'CRITICAL': 'red,bg_white',
     }
 
-    # Formatter for INFO level
-    info_formatter = colorlog.ColoredFormatter(
-        fmt='%(log_color)s%(asctime)s [INFO] %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
-        log_colors=log_colors
-    )
-
-    # Formatter for WARNING level
-    warning_formatter = colorlog.ColoredFormatter(
-        fmt='%(log_color)s%(asctime)s [WARNING] %(message)s\n    ⤷ In %(module)s',
+    # Formatter for INFO and WARNING levels
+    simple_formatter = colorlog.ColoredFormatter(
+        fmt='%(log_color)s[%(asctime)s, %(levelname)s] %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
         log_colors=log_colors
     )
 
     # Formatter for DEBUG, ERROR, and CRITICAL levels
     detailed_formatter = colorlog.ColoredFormatter(
-        fmt='%(log_color)s%(asctime)s [%(levelname)s] %(message)s\n    ⤷ In %(module)s%(classname)s.%(funcName)s:%('
-            'lineno)d',
+        fmt='%(log_color)s%(asctime)s [%(levelname)s] %(message)s\n    ⤷ In %(module)s:%(lineno)d, %(funcName)s',
         datefmt='%Y-%m-%d %H:%M:%S',
         log_colors=log_colors
     )
 
     class CustomHandler(logging.StreamHandler):
         def format(self, record):
-            if record.levelno == logging.INFO:
-                return info_formatter.format(record)
-            elif record.levelno == logging.WARNING:
-                return warning_formatter.format(record)
-            else:
-                if record.classname:
-                    record.classname = f".{record.classname}"
-                else:
-                    record.classname = ""
-                return detailed_formatter.format(record)
+            if record.levelno in (logging.INFO, logging.WARNING):
+                return simple_formatter.format(record)
+            return detailed_formatter.format(record)
 
     handler = CustomHandler()
 
