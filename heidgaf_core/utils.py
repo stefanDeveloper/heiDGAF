6003ca89769824eb5a3a14c967d2d6b38add5ab6
lamr02n <94921205+lamr02n@users.noreply.github.com>
2024-06-30 21:25:25 +0200

    adding log messages for utils.py

--- heidgaf_core/utils.py
+++ heidgaf_core/utils.py
@@ -13,19 +13,27 @@ logger = logging.getLogger(__name__)
 
 
 def validate_host(host) -> ipaddress.IPv4Address | ipaddress.IPv6Address:
+    logger.debug(f"Validating host IP address {host}...")
     try:
         host = ipaddress.ip_address(host)
     except ValueError:
+        logger.error(f"Host {host} is invalid.")
         raise ValueError(f"Invalid host: {host}")
+
+    logger.debug(f"Host {host} is valid.")
     return host
 
 
 def validate_port(port: int) -> int:
+    logger.debug(f"Validating port {port}...")
     if not isinstance(port, int):
         raise TypeError
 
     if not (1 <= port <= 65535):
+        logger.error(f"Port {port} is invalid.")
         raise ValueError(f"Invalid port: {port}")
+
+    logger.debug(f"Port {port} is valid.")
     return port
 
 
@@ -33,4 +41,4 @@ def kafka_delivery_report(err: None | KafkaError, msg: None | Message):
     if err:
         logger.warning('Message delivery failed: {}'.format(err))
     else:
-        logger.info('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+        logger.info('Message delivered to topic={} [partition={}]'.format(msg.topic(), msg.partition()))
