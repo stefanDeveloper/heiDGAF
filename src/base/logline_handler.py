8741da91c0244b89cdd889141031126b29ac9f34
Manuel Fuchs <manuel-f-02@proton.me>
2024-09-22 12:27:45 +0200

    Add docstrings to logline_handler.py

--- src/base/logline_handler.py
+++ src/base/logline_handler.py
@@ -13,27 +13,67 @@ REQUIRED_FIELDS = ["timestamp", "status_code", "client_ip", "record_type"]
 
 
 class FieldType:
+    """
+    Base class for types of fields.
+    """
+
     def __init__(self, name: str):
         self.name = name
 
     def validate(self, value) -> bool:
+        """
+        Validates the input value. Implementation in inheriting classes.
+
+        Args:
+            value: The value to be validated
+
+        Raises:
+            NotImplementedError
+        """
         raise NotImplementedError
 
 
 class RegEx(FieldType):
+    """
+    A :cls:`RegEx` object takes a name, and a pattern, which a value needs to have the format of.
+    """
+
     def __init__(self, name: str, pattern: str):
         super().__init__(name)
         self.pattern = re.compile(r"{}".format(pattern))
 
     def validate(self, value) -> bool:
+        """
+        Validates the input value.
+
+        Args:
+            value: The value to be validated
+
+        Returns:
+            True if the value is valid, False otherwise
+        """
         return True if re.match(self.pattern, value) else False
 
 
 class IpAddress(FieldType):
+    """
+    An :cls:`IpAddress` object takes only a name. It is used for IP addresses, and checks in the :meth:`validate` method
+    if the value is a correct IP address.
+    """
+
     def __init__(self, name):
         super().__init__(name)
 
     def validate(self, value) -> bool:
+        """
+        Validates the input value.
+
+        Args:
+            value: The value to be validated
+
+        Returns:
+            True if the value is valid, False otherwise
+        """
         try:
             validate_host(value)
         except ValueError:
@@ -43,6 +83,13 @@ class IpAddress(FieldType):
 
 
 class ListItem(FieldType):
+    """
+    A :cls:`ListItem` object takes a name, and two lists: The
+    ``allowed_list`` contains all values, that the :cls:`ListItem` is allowed to have, and therefore are not sorted out.
+    The ``relevant_list`` must contain fields that are also in ``allowed_list`` and that are relevant for further
+    inspection. These are filtered in the Prefilter stage.
+    """
+
     def __init__(self, name: str, allowed_list: list, relevant_list: list):
         super().__init__(name)
         self.allowed_list = allowed_list
@@ -53,6 +100,15 @@ class ListItem(FieldType):
         self.relevant_list = relevant_list
 
     def validate(self, value) -> bool:
+        """
+        Validates the input value.
+
+        Args:
+            value: The value to be validated
+
+        Returns:
+            True if the value is valid, False otherwise
+        """
         return True if value in self.allowed_list else False
 
     # TODO: Add method to check if value in relevant list
@@ -60,6 +116,11 @@ class ListItem(FieldType):
 
 # TODO: Test
 class LoglineHandler:
+    """
+    Stores the configuration format of loglines and can be used to validate a given logline, i.e. checks if the given
+    logline has the format given in the configuration. Can also return the validated logline as dictionary.
+    """
+
     def __init__(self):
         self.instances_by_name = {}
         self.instances_by_position = {}
@@ -80,6 +141,17 @@ class LoglineHandler:
                 raise ValueError("Not all needed fields are set in the configuration")
 
     def validate_logline(self, logline: str) -> bool:
+        """
+        Validates the given input logline by checking if the number of fields is correct as well as all the fields, by
+        calling the :meth:`validate` method of each field. If the logline is incorrect, it shows an error with the
+        incorrect fields being highlighted.
+
+        Args:
+            logline (str): Logline as string to be validated
+
+        Returns:
+            True if the logline contains correct fields in the configured format, False otherwise
+        """
         parts = logline.split()
 
         # check number of entries
@@ -99,7 +171,7 @@ class LoglineHandler:
 
             for i in range(self.number_of_fields):
                 if valid_values[i]:
-                    error_line += len(parts[i]) * " "  # keep all valid fields normal
+                    error_line += len(parts[i]) * " "  # keep all valid fields unchanged
                 else:
                     error_line += len(parts[i]) * "^"  # underline all wrong fields
                 error_line += " "
@@ -110,6 +182,16 @@ class LoglineHandler:
         return True
 
     def validate_logline_and_get_fields_as_json(self, logline: str) -> dict:
+        """
+        Validates the fields and returns them as dictionary, with the names of the fields as key, and the field value
+        as value.
+
+        Args:
+            logline (str): Logline as string to be validated
+
+        Returns:
+            Dictionary of field names as keys and field values as value
+        """
         if not self.validate_logline(logline):
             raise ValueError("Incorrect logline, validation unsuccessful")
 
@@ -121,7 +203,17 @@ class LoglineHandler:
 
         return return_dict.copy()
 
-    def _create_instance_from_list_entry(self, field_list: list):
+    @staticmethod
+    def _create_instance_from_list_entry(field_list: list):
+        """
+        Extracts the information from the ``field_list`` to generate one instance of the specified type.
+
+        Args:
+            field_list (list): List of field name, type and additional fields
+
+        Returns:
+            Generated instance with the name, type and additional parameters given in the ``field_list``
+        """
         len_of_field_list = len(field_list)
 
         if len_of_field_list < 2 or not isinstance(field_list[0], str):
