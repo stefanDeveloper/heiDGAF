4b3f15c22d916191d359e406fb6c0682de56b6d9
Stefan Machmeier <stefan-machmeier@outlook.com>
2024-09-24 17:37:39 +0200

    Update config for anomalies

--- src/mock/generator.py
+++ src/mock/generator.py
@@ -17,7 +17,8 @@ if __name__ == "__main__":
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.connect((str("127.0.0.1"), 9998))
         while True:
-            logline = generate_dns_log_line()
-            client_socket.send(logline.encode("utf-8"))
-            logger.info(f"Sent logline: {logline}")
+            for i in range(0, 10):
+                logline = generate_dns_log_line()
+                client_socket.send(logline.encode("utf-8"))
+                logger.info(f"Sent logline: {logline}")
             time.sleep(0.1)
