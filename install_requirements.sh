252f4fc1f3120145180405f1f4217412ae8aebd3
Manuel Fuchs <manuel-f-02@proton.me>
2024-10-16 16:00:18 +0200

    Add check if python and pip are installed

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Check if pip is available
+if ! command -v pip &> /dev/null
+then
+    echo "pip could not be found, please install Python and pip first."
+    exit
+fi
+
 # Find all requirements*.txt files in the current directory
 for req_file in $(ls requirements/requirements.*.txt); do
     echo "Installing from $req_file..."
