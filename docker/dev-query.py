8d4b345f272ec78a2af271151a914c17efc263dc
Manuel Fuchs <manuel-f-02@proton.me>
2024-12-09 22:01:02 +0100

    Add a dev-query module for quick database checks

--- docker/dev-query.py
+++ docker/dev-query.py
@@ -1,10 +1,51 @@
+import datetime
+import os
+import sys
+import uuid
+
 import clickhouse_connect
 
-QUERY_TABLE = "dns_loglines"
+sys.path.append(os.getcwd())
+from src.base.data_classes.clickhouse_connectors import TABLE_NAME_TO_TYPE
 
-client = clickhouse_connect.get_client(host="172.27.0.11", port=8123)
 
-result = client.query(f"SELECT * FROM {QUERY_TABLE};")
+def get_tables():
+    tables = {}
 
-for row in result.result_rows:
-    print(row)
+    for table_name in TABLE_NAME_TO_TYPE:
+        tables[table_name] = []
+
+    return tables
+
+
+def query_once(client, tables):
+    for table_name in tables.keys():
+        tables[table_name] = client.query(f"SELECT * FROM {table_name};")
+
+    return tables
+
+
+def main():
+    client = clickhouse_connect.get_client(host="172.27.0.11", port=8123)
+    tables = get_tables()
+
+    client.insert(
+        "server_logs",
+        [[uuid.uuid4(), datetime.datetime.now(), "This is a logline"]],
+        ["message_id", "timestamp_in", "message_text"],
+    )
+
+    results = query_once(client, tables)
+
+    for key in results:
+        print(f"'{key}':")
+
+        if results[key].result_rows:
+            for row in results[key].result_rows:
+                print("\t", row)
+        else:
+            print("\t -")
+
+
+if __name__ == "__main__":
+    main()
