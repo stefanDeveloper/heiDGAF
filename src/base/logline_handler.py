9b6f9bc33dd393cdacbc9ff7cdd749a444ff19a2
Manuel F <manuel-f-02@proton.me>
2025-01-03 13:35:07 +0100

    Clean up logline_handler.py

--- src/base/logline_handler.py
+++ src/base/logline_handler.py
@@ -176,11 +176,12 @@ class LoglineHandler:
             True if the logline contains correct fields in the configured format, False otherwise
         """
         parts = logline.split()
+        number_of_entries = len(parts)
 
         # check number of entries
-        if len(parts) != self.number_of_fields:
+        if number_of_entries != self.number_of_fields:
             logger.warning(
-                f"Logline contains {len(parts)} value(s), not {self.number_of_fields}."
+                f"Logline contains {number_of_entries} value(s), not {self.number_of_fields}."
             )
             return False
 
@@ -225,9 +226,8 @@ class LoglineHandler:
         return return_dict.copy()
 
     def validate_logline_and_get_fields_as_json(self, logline: str) -> dict:
-        """
-        Validates the fields and returns them as dictionary, with the names of the fields as key, and the field value
-        as value.
+        """Validates the fields and returns them as dictionary, with the names of the fields as key, and the field
+        value as value.
 
         Args:
             logline (str): Logline as string to be validated
