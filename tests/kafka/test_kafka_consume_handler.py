9113bb3771d7465ecdbbf9d14bff6f8d7413461d
Manuel F <manuel-f-02@proton.me>
2025-02-19 19:13:26 +0100

    Update test_kafka_consume_handler.py

--- tests/kafka/test_kafka_consume_handler.py
+++ tests/kafka/test_kafka_consume_handler.py
@@ -2,7 +2,11 @@ import json
 import unittest
 from unittest.mock import patch, MagicMock
 
-from src.base.kafka_handler import KafkaConsumeHandler, KafkaMessageFetchException
+from src.base.kafka_handler import (
+    KafkaConsumeHandler,
+    KafkaMessageFetchException,
+    TooManyFailedAttemptsError,
+)
 
 
 class TestInit(unittest.TestCase):
@@ -53,6 +57,53 @@ class TestInit(unittest.TestCase):
 
         mock_consumer.assert_called_once_with(expected_conf)
         mock_consumer_instance.subscribe.assert_called_once()
+
+    @patch("src.base.kafka_handler.CONSUMER_GROUP_ID", "test_group_id")
+    @patch(
+        "src.base.kafka_handler.KAFKA_BROKERS",
+        [
+            {
+                "hostname": "127.0.0.1",
+                "port": 9999,
+            },
+            {
+                "hostname": "127.0.0.2",
+                "port": 9998,
+            },
+            {
+                "hostname": "127.0.0.3",
+                "port": 9997,
+            },
+        ],
+    )
+    @patch(
+        "src.base.kafka_handler.KafkaConsumeHandler._all_topics_created",
+        return_value=False,
+    )
+    @patch("src.base.kafka_handler.AdminClient")
+    @patch("src.base.kafka_handler.Consumer")
+    def test_init_unsuccessful(
+        self, mock_consumer, mock_admin_client, mock_all_topics_created
+    ):
+        # Arrange
+        mock_consumer_instance = MagicMock()
+        mock_consumer.return_value = mock_consumer_instance
+
+        expected_conf = {
+            "bootstrap.servers": "127.0.0.1:9999,127.0.0.2:9998,127.0.0.3:9997",
+            "group.id": "test_group_id",
+            "enable.auto.commit": False,
+            "auto.offset.reset": "earliest",
+            "enable.partition.eof": True,
+        }
+
+        # Act
+        with self.assertRaises(TooManyFailedAttemptsError):
+            KafkaConsumeHandler(topics="test_topic")
+
+        # Assert
+        mock_consumer.assert_called_once_with(expected_conf)
+        mock_consumer_instance.subscribe.assert_not_called()
 
     @patch("src.base.kafka_handler.CONSUMER_GROUP_ID", "test_group_id")
     @patch(
