a29b9007a5bd94913ea12baf735422d9febf7073
Manuel F <manuel-f-02@proton.me>
2025-05-27 09:38:01 +0200

    Add benchmarking/src/test_types directory

---
+++ benchmarking/src/setup_config.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+import yaml
+
+sys.path.append(os.getcwd())
+from src.base.log_config import get_logger
+from src.base.utils import setup_config
+
+logger = get_logger()
+config = setup_config()
+
+CONFIG_FILEPATH = os.path.join(
+    os.path.dirname(__file__), "../benchmark_test_config.yaml"
+)
+
+
+def setup_config():
+    """
+    Loads the configuration data from the configuration file and returns it as the corresponding Python object.
+
+    Returns:
+         Configuration data as corresponding Python object
+
+    Raises:
+        FileNotFoundError: Configuration file could not be opened
+    """
+    try:
+        logger.debug(
+            f"Opening benchmark test configuration file at {CONFIG_FILEPATH}..."
+        )
+        with open(CONFIG_FILEPATH, "r") as file:
+            configuration = yaml.safe_load(file)
+    except FileNotFoundError:
+        logger.critical(f"File {CONFIG_FILEPATH} does not exist. Aborting...")
+        raise
+
+    logger.debug("Configuration file successfully opened and information returned.")
+    return configuration
