f368e851cdb62d852c462331ccce7fe6e5c828a4
maldwg <maxludwig3@t-online.de>
2025-08-05 14:46:15 +0200

    update dashboard and fix loglines count again

--- docker/create_tables/loglines.sql
+++ docker/create_tables/loglines.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS loglines (
     logline_id UUID NOT NULL,
     timestamp DateTime64(6) NOT NULL,
+    subnet_id String NOT NULL,
     src_ip String NOT NULL,
     additional_fields String
 )
