7ab7bd5bd6341419315c40e225daa4d316cea414
Manuel Fuchs <manuel-f-02@proton.me>
2025-08-18 17:54:13 +0200

    Add tests for SingleIntervalTest (1)

---
+++ tests/benchmark/test_single_interval.py
@@ -0,0 +1,155 @@
+import datetime
+import unittest
+from unittest.mock import patch, Mock, call, ANY
+
+from confluent_kafka import KafkaException
+
+from benchmarking.src.test_types.base import SingleIntervalTest
+
+
+class TestInit(unittest.TestCase):
+    def test_successful(self):
+        # Arrange
+        test_full_length_in_minutes = 17.3
+        test_messages_per_second = 70
+
+        with patch(
+            "benchmarking.src.test_types.base.SingleIntervalTest._SingleIntervalTest__get_total_message_count"
+        ) as mock_get_total_message_count, patch(
+            "benchmarking.src.test_types.base.BaseTest.__init__"
+        ) as mock_base_test_init:
+            mock_get_total_message_count.return_value = 150
+
+            # Act
+            sut = SingleIntervalTest(
+                test_full_length_in_minutes, test_messages_per_second
+            )
+
+        # Assert
+        self.assertEqual(sut.full_length_in_minutes, 17.3)
+        self.assertEqual(sut.messages_per_second, 70)
+
+        mock_base_test_init.assert_called_once_with(
+            is_interval_based=False,
+            total_message_count=150,
+        )
+
+
+class TestExecuteCore(unittest.TestCase):
+    def setUp(self):
+        """Mocks the logger to deactivate logs in test run."""
+        patcher = patch("benchmarking.src.test_types.base.logger")
+        self.mock_logger = patcher.start()
+        self.addCleanup(patcher.stop)
+
+    @patch("benchmarking.src.test_types.base.PRODUCE_TO_TOPIC", "test_topic")
+    def test_successful(self):
+        # Arrange
+        test_full_length_in_minutes = 5 / 60  # 5 seconds
+        test_messages_per_second = 100
+
+        with patch("benchmarking.src.test_types.base.BaseTest.__init__"):
+            sut = SingleIntervalTest(
+                full_length_in_minutes=test_full_length_in_minutes,
+                messages_per_second=test_messages_per_second,
+            )
+            sut.kafka_producer = Mock()
+            sut.dataset_generator = Mock()
+            sut.custom_fields = {"message_count": Mock()}
+            sut.progress_bar = Mock()
+
+        with patch("benchmarking.src.test_types.base.datetime") as mock_datetime, patch(
+            "benchmarking.src.test_types.base.time.sleep"
+        ) as mock_time_sleep, patch(
+            "benchmarking.src.test_types.base.SingleIntervalTest._get_time_elapsed"
+        ) as mock_get_time_elapsed:
+            mock_datetime.now.side_effect = [
+                datetime.datetime(2025, 1, 1, 12, 0, 0, 0),
+                datetime.datetime(2025, 1, 1, 12, 0, 1, 0),
+                datetime.datetime(2025, 1, 1, 12, 0, 4, 0),
+                datetime.datetime(2025, 1, 1, 12, 0, 6, 0),
+            ]
+            mock_get_time_elapsed.side_effect = [
+                datetime.timedelta(seconds=3),
+                datetime.timedelta(seconds=4.5),
+            ]
+
+            # Act
+            sut._execute_core()
+
+        # Assert
+        self.assertEqual(len(sut.kafka_producer.produce.mock_calls), 2)
+        sut.kafka_producer.produce.assert_called_with("test_topic", ANY)
+
+        expected_update_mapping_calls = [
+            call(current_message_count=0),
+            call(current_message_count=1),
+        ]
+        self.assertEqual(
+            len(sut.custom_fields["message_count"].update_mapping.mock_calls), 2
+        )
+        sut.custom_fields["message_count"].update_mapping.assert_has_calls(
+            expected_update_mapping_calls
+        )
+
+        expected_progress_bar_update_calls = (call(60.0), call(90.0))
+        self.assertEqual(
+            len(sut.progress_bar.update.mock_calls), 3
+        )  # two inside loop, one outside (to 100)
+        sut.progress_bar.update.assert_has_calls(expected_progress_bar_update_calls)
+
+        self.assertEqual(len(mock_time_sleep.mock_calls), 2)
+        mock_time_sleep.assert_called_with(0.01)
+
+    @patch("benchmarking.src.test_types.base.PRODUCE_TO_TOPIC", "test_topic")
+    def test_including_kafka_error(self):
+        # Arrange
+        test_full_length_in_minutes = 5 / 60  # 5 seconds
+        test_messages_per_second = 100
+
+        with patch("benchmarking.src.test_types.base.BaseTest.__init__"):
+            sut = SingleIntervalTest(
+                full_length_in_minutes=test_full_length_in_minutes,
+                messages_per_second=test_messages_per_second,
+            )
+            sut.kafka_producer = Mock()
+            sut.dataset_generator = Mock()
+            sut.custom_fields = {"message_count": Mock()}
+            sut.progress_bar = Mock()
+
+            sut.kafka_producer.produce.side_effect = [KafkaException, None]
+
+        with patch("benchmarking.src.test_types.base.datetime") as mock_datetime, patch(
+            "benchmarking.src.test_types.base.time.sleep"
+        ) as mock_time_sleep, patch(
+            "benchmarking.src.test_types.base.SingleIntervalTest._get_time_elapsed"
+        ) as mock_get_time_elapsed:
+            mock_datetime.now.side_effect = [
+                datetime.datetime(2025, 1, 1, 12, 0, 0, 0),
+                datetime.datetime(2025, 1, 1, 12, 0, 1, 0),
+                datetime.datetime(2025, 1, 1, 12, 0, 4, 0),
+                datetime.datetime(2025, 1, 1, 12, 0, 6, 0),
+            ]
+            mock_get_time_elapsed.side_effect = [datetime.timedelta(seconds=4.5)]
+
+            # Act
+            sut._execute_core()
+
+        # Assert
+        self.assertEqual(len(sut.kafka_producer.produce.mock_calls), 2)
+        sut.kafka_producer.produce.assert_called_with("test_topic", ANY)
+
+        sut.custom_fields["message_count"].update_mapping.assert_called_once_with(
+            current_message_count=0
+        )
+
+        expected_progress_bar_update_calls = (call(90.0), call(100.0))
+        self.assertEqual(len(sut.progress_bar.update.mock_calls), 2)
+        sut.progress_bar.update.assert_has_calls(expected_progress_bar_update_calls)
+
+        self.assertEqual(len(mock_time_sleep.mock_calls), 2)
+        mock_time_sleep.assert_called_with(0.01)
+
+
+if __name__ == "__main__":
+    unittest.main()
